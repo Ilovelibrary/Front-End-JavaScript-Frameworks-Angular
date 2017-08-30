a7f9ff6727f72a71faeb18b090fc63a4739d5a86
Pu <p5gao@ucsd.edu>
2017-08-30 03:23:29 -0700

    Assignment 3

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
+import { Comment } from '../shared/comment';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { DishService } from '../services/dish.service';
 
@@ -20,18 +22,87 @@ export class DishdetailComponent implements OnInit {
   dishIds: number[];
   prev: number;
   next: number;
+  
+  commentForm: FormGroup;
+  dishComment: Comment;
+
+  formErrors = {
+    'comment':'',
+    'author':''
+  }
+  validationMessages = {
+    'comment': {
+      'required':      'Comment is required.',
+      'minlength':     'Comment must be at least 2 characters long.'
+    },
+    'author': {
+      'required':      'Author Name is required.',
+      'minlength':     'Author Name must be at least 2 characters long.',
+      'maxlength':     'Author Name cannot be more than 25 characters long.'
+    }
+  }
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private fb: FormBuilder) { 
+      this.createForm();
+    }
 
   ngOnInit() {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
-    let id = +this.route.snapshot.params['id'];
-    this.dishservice.getDish(id).subscribe(dish => this.dish=dish);
     this.route.params
       .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
       .subscribe(dish => {this.dish=dish; this.setPrevNext(dish.id)})
+  }
+
+  createForm(): void {
+    this.commentForm = this.fb.group({
+      rating: 5,
+      comment: ['', [Validators.required,Validators.minLength(2)]],
+      author: ['', [Validators.required,Validators.minLength(2),Validators.maxLength(25)]],
+      date: ''
+    });
+    this.commentForm.valueChanges.subscribe(data => this.onValueChanged(data));
+    this.onValueChanged();
+  }
+
+  onValueChanged(data?:any) {
+    const form=this.commentForm;
+    if (form.get('author').valid && form.get('comment').valid) {
+      this.dishComment = this.commentForm.value;
+    }
+    else {
+      this.dishComment = {
+        rating: 0,
+        comment: '',
+        author: '',
+        date: ''
+    };
+    }
+  
+    for (const field in this.formErrors) {
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key]+' ';
+        }
+      }
+    }
+  }
+
+  onSubmit() {
+    this.dishComment.date = Date();
+    this.dish.comments.push(this.dishComment);
+    console.log(this.dishComment);
+    this.commentForm.reset({
+      rating: 5,
+      comment: '',
+      author: '',
+      date: ''
+    });
   }
 
   setPrevNext(dishId: number) {
